efa298309a0fc0a1b3d2f98661a27dce91017782
Samanio1989 <141902886+Samanio1989@users.noreply.github.com>
2026-08-28 14:04:37 +0700

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-/**
- * ใส่ URL Web App ของ Google Apps Script ที่ Deploy แล้ว
- * ต้องเป็น URL ที่ลงท้ายด้วย /exec
- *
- * ตัวอย่าง:
- * const API_URL = 'https://script.google.com/macros/s/XXXXXXXXXXXX/exec';
- */
-const API_URL = 'PUT_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE';

51ba9010d27a54fc963c2741a68d4a753d8a368b
Samanio1989 <141902886+Samanio1989@users.noreply.github.com>
2026-08-28 13:26:30 +0700

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,8 @@
+/**
+ * ใส่ URL Web App ของ Google Apps Script ที่ Deploy แล้ว
+ * ต้องเป็น URL ที่ลงท้ายด้วย /exec
+ *
+ * ตัวอย่าง:
+ * const API_URL = 'https://script.google.com/macros/s/XXXXXXXXXXXX/exec';
+ */
+const API_URL = 'https://script.google.com/macros/s/AKfycbx7k7muZOYHNLJ_U6IXNIY6U0zmmL3nnyqexdNkLB0kVsc___9m6_46aI_jPsQ5nSw/exec';
